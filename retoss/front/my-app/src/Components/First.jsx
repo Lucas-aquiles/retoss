bdf066a3e265bc733c69fd2880bfe216ad926968
Lucas Manuel <49735490+Lucas-aquiles@users.noreply.github.com>
2022-07-24 04:19:59 -0300

    x

--- retoss/front/my-app/src/Components/First.jsx
+++ retoss/front/my-app/src/Components/First.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import './First.css'
-
+import React from "react";
+import "./First.css";
 
 const First = () => {
   return (
-    <div className='FirstContainer'>
+    <div className="FirstContainer">
+      <div>
+        <h1>10,000+ of our users love our products. </h1>
 
-<div > 
-    <h1>10,000+ of our users love our products.   </h1>
-
-    <p>We only provide great products combined with excellent customer service.
-    See what our satisfied customers are saying about our services.
-   </p>
-
-   </div>
+        <p>
+          We only provide great products combined with excellent customer
+          service. See what our satisfied customers are saying about our
+          services.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default First
+export default First;
--- retoss/front/my-app/src/Components/Third.jsx
+++ retoss/front/my-app/src/Components/Third.jsx
@@ -1,75 +1,65 @@
 import React from "react";
-import './Third.css'
-import   colton from '../images/image-colton.jpg'
-import   irene from '../images/image-irene.jpg'
-import   anne from '../images/image-anne.jpg'
-
+import "./Third.css";
+import colton from "../images/image-colton.jpg";
+import irene from "../images/image-irene.jpg";
+import anne from "../images/image-anne.jpg";
 
 const Third = () => {
   return (
     <div className="ThirdContainer">
       <div className="Commentary Commentarypart1 ">
-        <div  className="part1">  
-        <img className="image" src={colton} alt="image"/> 
-        <div>    
-        <h3> Colton Smith </h3>
-        <h4> Verified Buyer</h4>
+        <div className="part1">
+          <img className="image" src={colton} alt="image" />
+          <div>
+            <h3> Colton Smith </h3>
+            <h4> Verified Buyer</h4>
+          </div>
         </div>
+
+        <div className="part2">
+          <p>
+            "We needed the same printed design as the one we had ordered a week
+            prior. Not only did they find the original order, but we also
+            received it in time. Excellent!"
+          </p>
         </div>
-
-<div className="part2">  
-        <p>
-          "We needed the same printed design as the one we had ordered a week
-          prior. Not only did they find the original order, but we also received
-          it in time. Excellent!"
-        </p>
       </div>
-
-      </div>
-
-
 
       <div className="Commentary  Commentarypart2">
-        <div  className="part1">  
-        <img className="image" src={irene} alt="image"/> 
-        <div>    
-        <h3> Irene Roberts </h3>
-        <h4> Verified Buyer</h4>
+        <div className="part1">
+          <img className="image" src={irene} alt="image" />
+          <div>
+            <h3> Irene Roberts </h3>
+            <h4> Verified Buyer</h4>
+          </div>
         </div>
+
+        <div className="part2">
+          <p>
+            "Customer service is always excellent and very quick turn around.
+            Completely delighted with the simplicity of the purchase and the
+            speed of delivery."
+          </p>
         </div>
-
-<div className="part2">  
-        <p>
-        "Customer service is always excellent and very quick turn around.
-          Completely delighted with the simplicity of the purchase and the speed
-          of delivery."
-        </p>
       </div>
-
-      </div>
-
-
-
 
       <div className="Commentary  ">
-        <div  className="part1">  
-        <img className="image" src={anne} alt="image"/> 
-        <div>    
-        <h3> Anne Wallace</h3>
-        <h4> Verified Buyer</h4>
+        <div className="part1">
+          <img className="image" src={anne} alt="image" />
+          <div>
+            <h3> Anne Wallace</h3>
+            <h4> Verified Buyer</h4>
+          </div>
         </div>
+
+        <div className="part2">
+          <p>
+            "Put an order with this company and can only praise them for the
+            very high standard. Will definitely use them again and recommend
+            them to everyone!"
+          </p>
         </div>
-
-<div className="part2">  
-        <p>
-        "Put an order with this company and can only praise them for the very
-          high standard. Will definitely use them again and recommend them to
-          everyone!"
-        </p>
       </div>
-
-      </div>
-
     </div>
   );
 };
