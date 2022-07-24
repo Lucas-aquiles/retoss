597a5e3e43ea5379d2cdcc56935cb1c8314bba30
Lucas Manuel <49735490+Lucas-aquiles@users.noreply.github.com>
2022-07-23 21:50:19 -0300

    changes

--- retoss/front/my-app/src/Components/Second.jsx
+++ retoss/front/my-app/src/Components/Second.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Second = () => {
   return (
-    <div>Second</div>
+    <div>
+<div>  
+<h3> Rated 5 Stars in Reviews  </h3>
+</div>
+
+<div>  
+  <h3>  Rated 5 Stars in Report Guru  </h3> 
+  </div>
+  
+  <div>  
+   <h3>  Rated 5 Stars in BestTech </h3>
+   </div>
+
+
+
+    </div>
   )
 }
 
